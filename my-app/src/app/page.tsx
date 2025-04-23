e18fa223e14456ab78e7aaf4283f2badc99bdb4f
Spexxl <snexxlorioh@outlook.com>
2025-04-23 19:42:15 -0300

    First Commit

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "./Components/Header";
+import Hero from "./Components/Home-Sections/Hero";
+
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <Header/>
+      <Hero/>
+    </div>
+  );
+}
